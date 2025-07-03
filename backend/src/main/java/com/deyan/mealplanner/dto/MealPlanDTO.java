fa605902dd8d558b990482ba7cea736c3c38599d
Deyan Papazov <deyanpapazov@gmail.com>
2025-07-03 23:41:37 +0300

    Added servings,index (for weekly plans) and sourceUrl to Meal constructor

--- backend/src/main/java/com/deyan/mealplanner/dto/MealPlanDTO.java
+++ backend/src/main/java/com/deyan/mealplanner/dto/MealPlanDTO.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public record MealPlanDTO(@JsonProperty("meals") List<Meal> meals,
                           @JsonProperty("nutrients") Nutrients nutrients) {
-    public record Meal(Long id,String title,int readyInMinutes, String imageType){}
+    public record Meal(Long index,Long id,String title,String imageType,int readyInMinutes,Integer servings,String sourceUrl){}
     public record Nutrients( BigDecimal calories,
                              BigDecimal protein,
                              BigDecimal fat,
