b8f58976ebe14d04fb0dbe8e62f646e92d658179
Deyan Papazov <deyanpapazov@gmail.com>
2025-07-05 17:21:42 +0300

    Added the needed RecipeService and RecipeController responsible for fetching and returning a recipe

---
+++ backend/src/main/java/com/deyan/mealplanner/controller/RecipeController.java
@@ -0,0 +1,21 @@
+package com.deyan.mealplanner.controller;
+
+import com.deyan.mealplanner.dto.RecipeDetailsDTO;
+import com.deyan.mealplanner.service.RecipeService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/recipes")
+public class RecipeController {
+    private final RecipeService recipeService;
+    public RecipeController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+    @GetMapping("/{id}")
+    public RecipeDetailsDTO getRecipeById(@PathVariable Long id) {
+        return recipeService.getRecipeDetailsById(id);
+    }
+}
