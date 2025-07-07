25ebd0b5d9ca2da5b958e6d2460aba20ec7d8959
Deyan Papazov <deyanpapazov@gmail.com>
2025-07-07 16:38:41 +0300

    Added achievements to the sql migration

---
+++ backend/src/main/resources/db/migration/V11__seed_achievements.sql
@@ -0,0 +1,7 @@
+INSERT INTO achievement (id, name, description) VALUES
+        (1, '1-day streak', 'Successfully log your weight for 1 day'),
+        (2, '7-day streak', 'Successfully log your weight for 1 week'),
+        (3,'30-day streak','Successfully log your weight for 1 month'),
+        (4,'10 logs','Add 10 weight entries'),
+        (5,'20 logs','Add 20 weight entries'),
+        (6,'30 logs','Add 30 weight entries');
